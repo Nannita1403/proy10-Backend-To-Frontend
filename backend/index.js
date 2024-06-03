31149e388b18dbc3b60c66b2f0407d6ea049c0ea
Nannita1403 <nannimagi@gmail.com>
2024-06-03 21:59:24 +0200

    .

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
-const express = require("express");
+const express = require ("express");
 const { connectDB } = require("./src/config/db");
+const mainRouter = require("./src/api/routes/main");
 
-const app = express(); 
+const app = express();
+
 connectDB();
+app.use(express.json());
 
-app.use("*", (req, res, next) => {
-    return res.status(404).json("Route not found")
-});
-app.listen(3000, () => {
+app.use("/api/v1", mainRouter);
+app.use("*", (req,res,next)=> {
+    return res.status(400).json("Route not found");
+})
+
+app.listen(3000, ()=> {
     console.log("http://localhost:3000");
 })
---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,43 @@
+const Event = require("../api/models/event");
+const User = require("../api/models/user");
+const { verificarLlave } = require("../utils/jwt");
+
+const isAuth = (req,res,next) => {
+    try { 
+        const token = req.headers.authorization?.replace("Beare ", "");
+        if(!token)
+            {return res.status(401).json("No autorizado");} 
+
+        const {id} = verificarLlave(token);
+        const user = User.findById(id);
+        user.password = null;  req.user = user; next();
+    } catch (error) {
+        return res.status(400).json("No estas autorizado para esta acción");
+    }
+};
+
+const isAdmin = async (req,res,next) =>{
+try {
+    req.user.role === "Admin"
+    ?(req.user.isAdmin=true)
+    :(req.user.isAdmin=false);
+    next();
+} catch (error) {
+    next(error);}
+}
+
+const isOrganizer = async (req,res,next) =>{
+try {
+    req.user.isOrganizer = false;
+    const eventId = req.params.id;
+    const event = await Event.findById(eventId).populate("organizer");
+    const organizerId = event.organizer.id;
+    if (req.user.id === organizerId){
+        req.user.isOrganizer = true;
+        }
+    next();
+} catch (error) {
+    next(error);}
+};
+
+module.exports ={isAuth, isAdmin, isOrganizer};
