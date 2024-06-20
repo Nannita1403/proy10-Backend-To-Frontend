3e238800d7882e9a5f693189361b8a792a554301
Nannita1403 <nannimagi@gmail.com>
2024-06-20 14:19:07 +0200

    fix positions

--- frontend/src/components/Events/Card/CardEvent.js
+++ frontend/src/components/Events/Card/CardEvent.js
@@ -7,16 +7,16 @@ export const EventCard = eventObject => {
   eventContainer.classList.add('flex-container');
   eventContainer.innerHTML = `
   <div class="img-container">
-    <img src="${eventObject.img ?? eventObject.game?.img[0]}" alt="${
-    eventObject.name
-  }"/>
+    <img src="${eventObject.img ?? eventObject.artist?.img[0]}" alt="${
+    eventObject.name}"/>
   </div>
   <div class="event-info flex-container">
     <div>
       <h4>${eventObject.name}</h4>
-      <p>${eventObject.game?.title || 'Juego a confirmar'}</p>
-      <p>${eventObject.date?.split('T')[0] || 'Fecha a confirmar'}</p>
       <p>${eventObject?.location || 'Ubicación a confirmar'}</p>
+      <p>${eventObject.artists?.name || 'Artistas a confirmar'}</p>
+      <p>${eventObject.date?.split('T')[0] || 'Fecha a confirmar'}</p>
+      <p>${eventObject.price || 'Precio a confirmar'}</p>
     </div>
   </div>
   `;
